dbdf425d1a18de7df1913cccb11b311af73bd079
Your Name <you@example.com>
2019-09-03 10:34:37 -0500

    initial commit

---
+++ pitch-perfect/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pitch_perfect.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
